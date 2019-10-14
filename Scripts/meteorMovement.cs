8dfa3fc0356e03c65714fa96425cb569c5cb9098
MartinBuilder <36473289+MartinBuilder@users.noreply.github.com>
2019-10-14 21:32:30 +0200

    Scripts
    Commit via Github site want Github Desktop en Bash werkte niet.

---
+++ Scripts/meteorMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteorMovement : MonoBehaviour {
+
+    public Transform target;
+    public float speed;
+    private int stop = 0;
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
